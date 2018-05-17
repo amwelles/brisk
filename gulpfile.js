c11dfc55c4c4e51cf79e6a45c889bb75d6f9fe13
Anna <anna.cantrell@crisp.agency>
2018-05-17 12:14:13 -0400

    adds imports to gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,12 @@ var minify = require('gulp-minify-css');
 var babelMinify = require('gulp-babel-minify');
 var rename = require('gulp-rename');
 var livereload = require('gulp-livereload');
+var atImport = require("postcss-import")
 
-// Do CSS things
+
 gulp.task('css', function() {
 	var plugins = [
+		atImport(),
 		cssnext()
 	];
 	return gulp.src('./style.css')
@@ -32,7 +34,7 @@ gulp.task('js', function() {
 // Watch things
 gulp.task('watch', function() {
 	livereload.listen();
-	gulp.watch(['./style.css', './functions.js'], gulp.parallel('css', 'js'));
+	gulp.watch(['./style.css', './functions.js', './imports/**/*.css'], gulp.parallel('css', 'js'));
 });
 
 gulp.task('default', gulp.series(gulp.parallel('css', 'js'), 'watch'));
